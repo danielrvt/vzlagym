ff7a2fac26b49d9f725f37a801ba69fa81f8b6ee
daniel <dani.tri88@gmail.com>
2013-11-24 09:53:56 -0430

    Agregado .htaccess

--- web/index.php
+++ web/index.php
@@ -8,16 +8,36 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 // definitions
 $app["debug"] = true;
 
 // routes
-$app->get("/", function () {
-    return json_encode(array(
-        "message" => "fuck you!"
-    ));
+$app->get("/gyms", function (Request $request) {
+
+    $data = array(
+        "reciever" => "you!",
+        "message" => "fuck"
+    );
+    $response = new Response(json_encode($data), 200, array('Content-type'=> 'application/json'));
+
+    return $response;
+});
+
+
+$app->get("/", function (Request $request) {
+
+    $data = array(
+        "url" => "/",
+        "message" => "fuck"
+    );
+    $response = new Response(json_encode($data), 200, array('Content-type'=> 'application/json'));
+
+    return $response;
 });
 
 
